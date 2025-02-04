2764b701bc9e4aedb9ee4beb600422df1aa1ad9d
Rosinês <X328084@prservicos.com.br>
2025-02-04 14:26:07 -0300

    add class transport  utizando abstractMethod

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
  
 import IndexAbstractFactory from "./Abstractfactory/notificationAbstractFactory";
 import IndexPaymentSytemAbstractFactory from "./Abstractfactory/paymentSytemAbstractFactory";
+import IndexTransportAbstractFactory from "./Abstractfactory/transportAbstractFactory";
 import indexFactory from "./FactoryMethod/reportFactory";
 import IndexTransport from "./FactoryMethod/transportFactory";
 import IndexUserManager from "./SRP/user/exercicio_1/userManager";
@@ -14,16 +15,19 @@ const user = new IndexUserManager();
 // console.log(user.init());
 
 
-const factory = new indexFactory()
-console.log(factory);
+// const factory = new indexFactory()
+// console.log(factory);
 
 
-const transport = new IndexTransport();
-console.log(transport.init('car'));
+// const transport = new IndexTransport();
+// console.log(transport.init('car'));
 
-const message = new IndexAbstractFactory()
-console.log(message.handleFamiliyAbastractMethos('email'))
+// const message = new IndexAbstractFactory()
+// console.log(message.handleFamiliyAbastractMethos('email'))
 
 
-const paymentSytem = new IndexPaymentSytemAbstractFactory()
-console.log(paymentSytem.handleProcess('payPal'))
+// const paymentSytem = new IndexPaymentSytemAbstractFactory()
+// console.log(paymentSytem.handleProcess('payPal'))
+
+const transportAbstractFactory = new IndexTransportAbstractFactory();
+console.log(transportAbstractFactory.init('uber'));
